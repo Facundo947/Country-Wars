f2a32a614f5e79b9c314cedca2be4a639c30ae63
Facundo947 <facundoarosmino@gmail.com>
2026-06-23 11:15:25 -0300

    Inspeccion de los codigos y prefabs

--- Assets/Scripts/Barra de vida Brasil.cs
+++ Assets/Scripts/Barra de vida Brasil.cs
@@ -1,6 +1,28 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class BarradevidaBrasil
+public class BarraDeVidaBrasil : MonoBehaviour
 {
-    
+    private Slider slider;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    public void CambiarVidaMaxima(float vidaMaxima)
+    {
+        slider.maxValue = vidaMaxima;
+    }
+
+    public void CambiarVidaActual(float cantidadVida)
+    {
+        slider.value = cantidadVida;
+    }
+
+    public void InicializarBarraDeVida(float cantidadVida)
+    {
+        CambiarVidaMaxima(cantidadVida);
+        CambiarVidaActual(cantidadVida);
+    }
 }
